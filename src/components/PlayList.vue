eacc431e275134e7775b4eb36a661715172e8413
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2020-03-03 23:18:11 +0100

    can render movie thumbnails

--- src/components/PlayList.vue
+++ src/components/PlayList.vue
@@ -1,37 +1,63 @@
 <template>
   <div class="scrolling-wrapper">
-    <div class="card">
+    <div class="card" v-for="(movie, ix) in playlists" :key="ix">
+        <img :src="movie.src" alt="" >
+    </div>
+    <!-- <div class="card">
       <h2>Card1</h2>
     </div>
     <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
-      <h2>Card</h2>
-    </div>
-    <div class="card">
       <h2>Cardx</h2>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    name: 'PlayList',
+    data() {
+        return{
+            playlists: [
+                {
+                    src: require('../assets/img/movie-posters/img1.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img2.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img3.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img4.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img5.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img6.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img7.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img8.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img9.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img10.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img11.jpeg')
+                },
+                {
+                    src: require('../assets/img/movie-posters/img12.jpeg')
+                }
+            ]
+        }
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -48,17 +74,18 @@ export default {};
 }
 
 .scrolling-wrapper {
-  height: 100px;
+  height: 145px;
   margin-bottom: 20px;
-  width: 60%;
+  width: 70%;
   padding: 5px;
+
   border: 2px solid black;
   border-radius: 10px;
 
   padding-left: 5px;
   padding-right: 10px;
   margin-top: 50px;
-  margin-left: 20%;
+  margin-left: 15%;
 
   align-items: center;
 
@@ -72,10 +99,11 @@ export default {};
 //   opacity: 0;
   border: 1px solid gray;
   width: 150px;
-  height: 75px;
+  height: auto;
   background: transparent;
 
   margin-left: 5px;
+  margin-right: 5px;
   border-radius: 10px;
   box-shadow: 0 1px 2px 0 rgba(32, 32, 32, 0.6);
 }
@@ -86,5 +114,11 @@ export default {};
 
 .card:last-child {
   margin-right: 15px;
+}
+
+.card img {
+    width: 100%; 
+    height: 130px;
+    border-radius: 5px;
 }
 </style>
